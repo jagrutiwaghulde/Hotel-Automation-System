2f36851d04b3d591ef66ebd60bf2724dd2945678
jagrutiwaghulde <waghuldejagruti@gmail.com>
2017-08-05 12:42:20 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,33 @@
+package org.pk.hotel.dao;
+
+import java.util.ArrayList;
+
+import org.pk.hotel.beans.Category;
+import org.pk.hotel.beans.Item;
+import org.pk.hotel.beans.Login;
+import org.pk.hotel.beans.OrderMaster;
+
+public class UserService
+{
+	
+	
+DBConnect dbconnect = new DBConnect();
+	
+	public ArrayList<Category> getAllCategorys(){
+		return dbconnect.getAllCategory();
+	}
+	public boolean checkLogin(Login login)
+	{
+		return  dbconnect.checkLogin(login);		
+	}
+	
+	public ArrayList<Item> getAllItems(int catId)
+	{
+		return dbconnect.getAllItemsByCategory(catId);
+	}
+	
+	public ArrayList<OrderMaster> getAllOrders(int tableNo){
+		return dbconnect.getAllOrders(tableNo);
+	}
+	
+}
